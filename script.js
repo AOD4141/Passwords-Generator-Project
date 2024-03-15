7197843df1de9f37de6808a3e27fe3aaba47e624
Adebayo <adenguk5251@gmail.com>
2024-03-15 01:32:38 +0000

    added the for loop function to run through series of characters and generate password

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ function passwordGenerator ()  {
     var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()+=£:;<>?[]|";
     var password = "";
 
-
+    for (var i= 0; i <= passwordLength; i++) {
+        var randomNumber = Math.floor(Math.random()* charset.length);
+    password += charset.substring(randomNumber, randomNumber +1);
+    }
+    
+    return password;
 }
