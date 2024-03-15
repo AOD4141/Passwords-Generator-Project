5df277e2cb7e992859b47ac729eea118a91cd4af
Adebayo <adenguk5251@gmail.com>
2024-03-15 01:31:29 +0000

    added the function to generate password

---
+++ script.js
@@ -0,0 +1,9 @@
+//generate function for password generation
+
+function passwordGenerator ()  {
+    var passwordLength = 18;
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()+=£:;<>?[]|";
+    var password = "";
+
+
+}
