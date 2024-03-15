999e0a815bd3a636ce7327a7f1d558788cb24d0e
Adebayo <adenguk5251@gmail.com>
2024-03-15 02:42:44 +0000

    added back script.js file

---
+++ .vscode/script.js
@@ -0,0 +1,26 @@
+function passwordGenerator ()  {
+    var passwordLength = 13;
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+    var password = "";
+
+for (var i= 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random()* charset.length);
+password += charset.substring(randomNumber, randomNumber +1);
+}
+
+return password;
+}
+
+console.log(passwordGenerator());
+
+window.onload = function () {
+document.getElementById("password").innerHTML = passwordGenerator();
+
+function writePassword() {
+var password = document.getElementById("password"); 
+password.innerText = passwordGenerator();
+}
+
+
+
+document.getElementById("generate").addEventListener("click",writePassword);
